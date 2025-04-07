ab3db66836c689ce03c55f3f4f3679f3db3f41c1
Aurore Agneray <aurore.agn@outlook.fr>
2025-04-07 16:53:07 +0200

    Ajout des propriétés Color et IconName dans l'entité Status et le DTO associé

--- DTOs/StatusGetDTO.cs
+++ DTOs/StatusGetDTO.cs
@@ -7,6 +7,22 @@ internal class StatusGetDTO
     [Column("id")]
     public required string Id { get; set; }
 
+    /// <summary>
+    /// Name of the status (Sent, Processing, Rejected, etc)
+    /// </summary>
     [Column("name")]
     public required string Name { get; set; }
+
+    /// <summary>
+    /// Color associated to the status for the front-end web app
+    /// </summary>
+    [Column("color")]
+    public required string Color { get; set; }
+
+    /// <summary>
+    /// Name of the Material Design Icon used into the front-end web app
+    /// https://pictogrammers.com/library/mdi/
+    /// </summary>
+    [Column("iconName")]
+    public required string IconName { get; set; }
 }
--- Entities/Status.cs
+++ Entities/Status.cs
@@ -15,5 +15,19 @@ internal class Status
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public required Guid Guid { get; set; }
 
+    /// <summary>
+    /// Name of the status (Sent, Processing, Rejected, etc)
+    /// </summary>
     public required string Name { get; set; }
+
+    /// <summary>
+    /// Color associated to the status for the front-end web app
+    /// </summary>
+    public required string Color { get; set; }
+
+    /// <summary>
+    /// Name of the Material Design Icon used into the front-end web app
+    /// https://pictogrammers.com/library/mdi/
+    /// </summary>
+    public required string IconName { get; set; }
 }
